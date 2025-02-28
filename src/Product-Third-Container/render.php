e9d4808a7a486c7bd5baf7be5bb10883f90369eb
Bryan Racoma <bryanracoma2002@gmail.com>
2025-02-28 13:15:37 +0800

    Customer Loyalty

---
+++ src/Product-Third-Container/render.php
@@ -0,0 +1,34 @@
+<section class="features-section">
+    <div class="container">
+        <header class="section-header">
+            <h2>Increase Customer Loyalty and Reduce Wait Times</h2>
+            <p>Transform your restaurant into a hub of efficiency and customer satisfaction.</p>
+        </header>
+
+        <div class="features-container">
+            <div class="feature-box">
+                <div class="icon-container">
+                    <img src="monitor.png">
+                </div>
+                <h3>Appealing Visual Menu</h3>
+                <p>Captivate your customers with a stunning menu featuring enticing images of your dishes.</p>
+            </div>
+
+            <div class="feature-box">
+                <div class="icon-container">
+                    <img src="book.png">
+                </div>
+                <h3>Easy Navigation</h3>
+                <p>Ensure your customers can quickly find their favorite meals. Get your restaurant online in just minutes with our straightforward and efficient setup process.</p>
+            </div>
+
+            <div class="feature-box">
+                <div class="icon-container">
+                    <img src="autorenew.png">
+                </div>
+                <h3>Real-Time Order Updates</h3>
+                <p>Keep your customers informed with instant notifications about their order status, enhancing their dining experience.</p>
+            </div>
+        </div>
+    </div>
+</section>
